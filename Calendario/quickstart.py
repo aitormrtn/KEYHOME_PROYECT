db0093b0cfc6dee4bfe902ab68310a9f9c469a56
aitormrtn <aitormartin1992@gmail.com>
2019-10-22 11:53:15 +0200

    Ordenación de documentos
    En esta actualización se ha procedido a ordenar de forma correcta los documentos apertura-puertas.py y quickstart.py por carpetas, con el fin de seguir un orden correcto en el desarrollo del proyecto

---
+++ Calendario/quickstart.py
@@ -0,0 +1,95 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Aquí leemos los próximos eventos
+
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Aquí tienes los próximos 10 eventos')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No tienes ningún evento futuro.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'], event['end'])#Con el start nos envía la fecha del evento, y en event['summary'] nos dan el contenido
+        
+
+    #A partir de aquí creamos el evento
+
+
+    # Refer to the Python quickstart on how to setup the environment:
+    # https://developers.google.com/calendar/quickstart/python
+    # Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
+    # stored credentials.
+
+    event = {
+    'summary': 'Nuevo evento de prueba',
+    'location': 'Campus de Fábrica de Armas, 45004 Toledo, España',
+    'description': 'Reunión con Francisco Moya.',
+    'start': {
+        'dateTime': '2019-10-22T09:00:00-07:00',
+        'timeZone': 'GMT+2',
+    },
+    'end': {
+        'dateTime': '2019-10-22T12:00:00-07:00',
+        'timeZone': 'GMT+2',
+    },
+    'recurrence': [
+        'RRULE:FREQ=DAILY;COUNT=2'
+    ],
+    'attendees': [
+        {'email': 'lpage@example.com'},
+        {'email': 'sbrin@example.com'},
+    ],
+    'reminders': {
+        'useDefault': False,
+        'overrides': [
+        {'method': 'email', 'minutes': 24 * 60},
+        {'method': 'popup', 'minutes': 10},
+        ],
+    },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print('Event created: %s' % (event.get('htmlLink')))
+    
+
+
+if __name__ == '__main__':
+    main()
