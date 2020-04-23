b11b9d2719e651809d562b271f962161ebc14643
aitormrtn <aitormartin1992@gmail.com>
2020-04-23 20:10:01 +0200

    Update bot.py

--- Conamm/bot.py
+++ Conamm/bot.py
@@ -28,7 +28,7 @@ def findat(msg):
 
 @bot.message_handler(commands=['start']) # welcome message handler
 def send_welcome(message):
-    bot.reply_to(message.chat.id, '(placeholder text)')
+    bot.reply_to(message, '(placeholder text)')
 
 @bot.message_handler(commands=['help']) # help message handler
 def send_welcome(message):
